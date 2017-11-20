41c63a5341a2cd3ed2a88b793b3371ad8b31371e
Brent <brentgammon94@gmail.com>
2017-11-20 11:13:57 +0000

    added welch t test

---
+++ statistical/statistical.js
@@ -0,0 +1,54 @@
+const jstat = require("jstat");
+const ttest = require("ttest");
+
+function welchTTest(dataset1, dataset2) {
+  return ttest(data1, data2, {
+    mu: 0,
+    varEqual: true,
+    alpha: 0.05,
+    alternative: "less"
+  }).valid();
+}
+
+function square(a) {
+  return a * a;
+}
+
+function mean(dataSet) {
+  return (
+    dataSet.reduce((a, b) => {
+      return a + b;
+    }) / dataSet.length
+  );
+}
+
+function standardDeviation(dataSet) {
+  return Math.sqrt(variance(dataSet));
+}
+
+function variance(dataSet) {
+  const average = mean(dataSet);
+  return mean(
+    dataSet.map(function(num) {
+      return Math.pow(num - average, 2);
+    })
+  );
+}
+
+function lowest(dataSet) {
+  return Math.min(...dataSet);
+}
+
+function highest(dataSet) {
+  return Math.max(...dataSet);
+}
+
+module.exports = {
+  welchTTest: welchTTest,
+  square: square,
+  mean: mean,
+  standardDeviation: standardDeviation,
+  variance: variance,
+  lowest: lowest,
+  highest: highest
+};
